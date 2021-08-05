42f8b9d806bb15ba7b4b6b08e6ba4b9964e4b100
Daniel Ulloa <danielulloam@gmail.com>
2021-08-05 12:41:48 -0500

    integracion bootstrap y componentes para la pagina

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactsComponent } from './contacts/contacts.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProjectsComponent,
+    ContactsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
