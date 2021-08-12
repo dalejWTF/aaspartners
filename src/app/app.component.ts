dacfa6c6ac9891b4feeae39a2ac6d5983041e4d7
Daniel Ulloa <danielulloam@gmail.com>
2021-08-12 16:52:47 -0500

    modulo ngBootstrap instalado para proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'aaspartners';
+  imagenes:any[]=[
+    {
+      url:'../assets/ejm.png'
+    },
+    {
+      url:'../assets/render_cocina.png'
+    },
+    {
+      url:'../assets/repisas.png'
+    }
+  ]
+
+  constructor(private _config:NgbCarouselConfig){
+    _config.interval=3000;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,6 +14,7 @@ import { ContactsComponent } from './contacts/contacts.component';
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
